b2b8f8ceb4bc9c6def135e18d7d1d2e711a37f4b
Adithya sudev <adithyasudev28@gmail.com>
2024-06-12 21:18:10 +0530

    ft30_401- day-1 nav

---
+++ frontend/my-app/src/components/Footer.jsx
@@ -0,0 +1,46 @@
+
+import { FaFacebookSquare, FaTwitterSquare, FaInstagram } from "react-icons/fa";
+// import "../Styles/Footer.css";
+import { Box, Flex, Text } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Box className="footer">
+      <Box className="footerContent" mb={6}>
+        <Box className="footerSection">
+          <Text fontWeight="bold" mb={3}>About Us</Text>
+          <Text mb={3}>About our website</Text>
+          <Text mb={3}>Contact Us</Text>
+          <Text mb={3}>Privacy Policy</Text>
+        </Box>
+        <Box className="footerSection">
+          <Text fontWeight="bold" mb={3}>
+            Recipes
+          </Text>
+          <Text mb={3}>View All Recipes</Text>
+          <Text mb={3}>Submit a Recipe</Text>
+          <Text mb={3}>Categories</Text>
+        </Box>
+        <Box className="footerSection">
+          <Text fontWeight="bold" mb={4}>Follow Us</Text>
+          <Flex alignItems="center" gap={3}>
+            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+              <FaFacebookSquare className="icon" />
+            </a>
+            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+              <FaTwitterSquare className="icon" />
+            </a>
+            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+              <FaInstagram className="icon" />
+            </a>
+          </Flex>
+        </Box>
+      </Box>
+      <Flex justifyContent="center" borderTop="1px solid #fff" pt="30px">
+        <Text>&copy; 2024 Cookbook</Text>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Footer;

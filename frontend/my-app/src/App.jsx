00d0721cb918139f029ff7b329bffb3e6d270799
PrasadPawar2729 <prasadpawarpp2002@gmail.com>
2024-06-14 22:15:26 +0530

    inprogress

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,18 +1,15 @@
 
 import './App.css'
-import { ChakraProvider } from '@chakra-ui/react'
-import Heading from './components/Heading.jsx'
-import Footer from './components/Footer.jsx'
+import { AllRoutes } from './Routes/AllRoutes'
+//import { ChakraProvider } from '@chakra-ui/react'
+// import Heading from './components/Heading.jsx'
+// import Footer from './components/Footer.jsx'
 
 function App() {
 
   return (
     <>
-      <ChakraProvider>
-      < Heading/>
-      <Footer/>
-   
-      </ ChakraProvider>
+      <AllRoutes/>
     </>
   )
 }
